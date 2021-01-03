213907a333872c3f00fc7562cfeb4fbb9ee6ef67
angro <mail@angro.org>
2021-01-03 19:47:27 +0300

    Update menu.py

--- menu/menu.py
+++ menu/menu.py
@@ -4,7 +4,9 @@ import win32ui
 import win32gui
 import win32con
 import win32api
-from PIL import Image, ImageColor
+from PIL import Image
+from pprint import pprint
+
 
 duhwnd = win32gui.FindWindow('', '')
 
@@ -14,43 +16,72 @@ win32gui.SendMessage(duhwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, win32
 time.sleep(.1)
 win32gui.SendMessage(duhwnd, win32con.WM_LBUTTONUP, 0x0, win32api.MAKELONG(10, 10))
 
-statedb = {l: list() for l in range(10)}
-scrolldb = dict()
+
 
 def cycle():
+    sleepz = 1
+    start_time = time.time()
+    time.sleep(sleepz)
 
-    state(duhwnd)
+    close(duhwnd, 0)
 
-    
+    print('')
+    print('time work:', (time.time() - start_time) - sleepz)
+
+
+
 def close(hwnd, lvl):
 
     while True:
-        state(hwnd)
-        if len([z[2] for z in statedb[lvl] if z[0]]) == 0: break
-        for i in range(len(statedb) - 1, -1, -1):
-            action = ([z[2] for z in statedb[i] if z[0]])
-            if len(action) > 0:
-                print(i)
-                for x, y in action:
-                    mouse(hwnd, (x, y), 'l')
-                    if i == 0: mouse(hwnd, (x - 10, y), 'l')
-                break
-            action.clear()
+        grid, scl = state(hwnd)
+        open_lvl = [[n for n, i in enumerate(grid) if i[1] and i[0] == l] for l in range(10)]
+        if not len(open_lvl[lvl]): break
+        open_lvl.reverse()
+        for l in open_lvl:
+            l.reverse()
+            for i in l:
+                mouse(hwnd, grid[i][3], 'l')
+            if len(l): break
 
-        time.sleep(.15)
-        
-        
+        time.sleep(.25)
+
+
+def scroll(hwnd, ud):
+
+    grid, scl = state(hwnd); print(scl)
+    hview, hscl, upline, downline, pos = scl
+    if not hscl: return
+    x, y = pos
+    mshift = 10
+
+    if ud == 'd':
+        shift = round((y + (hscl / hview) * downline) + (hscl / 25))
+    elif ud == 'u':
+        shift = 120
+    else:
+        shift = 0
+
+    win32gui.SendMessage(hwnd, win32con.WM_MOUSEMOVE, 0x0, win32api.MAKELONG(x, y + mshift))
+
+    win32gui.SendMessage(hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, win32api.MAKELONG(x, y + mshift))
+    time.sleep(.1)
+    win32gui.SendMessage(hwnd, win32con.WM_MOUSEMOVE, win32con.MK_LBUTTON, win32api.MAKELONG(x, shift + mshift))
+    time.sleep(.1)
+    win32gui.SendMessage(hwnd, win32con.WM_LBUTTONUP, 0x0, win32api.MAKELONG(x, shift + mshift))
+
+    # win32gui.SendMessage(hwnd, win32con.WM_MOUSEMOVE, 0x0, win32api.MAKELONG(x + 5, shift))
+
+
 def state(hwnd):
 
     c_line = (0x142027, 0x1C343C)
     c_arrow = (0xBB9F5B, 0xC0A765, 0xC2AC6A)
     c_bg = (0x000000, 0x142027, 0x1C343C)
-    c_sud = (0xB6DFED, )
-    c_sp = (0x547A87, 0xB6DFED)
+    c_scl = (0x547A87, 0xB6DFED)
     x_item = 360
-    x_scroll = 408
     field = (243, 418, 120, 680)  # sx, ex, sy, ey
     x_line = (7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47)
+    # ! magic number str 80, str 125
 
     rawimg = screen(hwnd, field)
     w = field[1] - field[0]; h = field[3] - field[2]
@@ -61,104 +92,126 @@ def state(hwnd):
     rows = tuple([rawpx[i:i+w] for i in range(0, len(rawpx), w)])
 
     # bottom lines
-    linepos = list(); add = False
+    bottom_lines = list(); add = False
     for rpos, row in enumerate(rows):
         row = row[:-25:-1]
         if len(set(c_line) & set(row)) > 0:
             add = True
         elif add:
             add = False
-            linepos.append(rpos - 1)
+            bottom_lines.append(rpos - 1)
 
-    if len(linepos) == 0:
+    if len(bottom_lines) == 0:
         print('no detect menu')
         exit(1)
 
-    # the nesting level of the menu
-    zlevel = list()
-    for l in linepos:
-        for c in c_line:
-            if c in rows[l]:
-                zlevel.append(x_line.index(rows[l].index(c)))
-                break
+    grig_lines = bottom_lines.copy()
+    if bottom_lines[0] < 15: bottom_lines.pop(0) # height 0 item
+    if grig_lines[0] >= 15: grig_lines.insert(0, 0)
+    grid_menu = tuple(zip(map(lambda p: p + 1 if p > 0 else p, grig_lines), grig_lines[1:]))
+    pix_menu = tuple([rawpx[a * w:b * w + 1] for a, b in grid_menu])
 
-    templine = linepos.copy()
-    if linepos[0] > 15: templine.insert(0, 0)
-    gridmenu = tuple(zip(map(lambda p: p + 1 if p > 0 else p, templine), templine[1:]))
-    pixmenu = tuple([rawpx[a*w:b*w+1] for a, b in gridmenu])
+    # the nesting level of the menu
+    rlevel = list()
+    for nl, l in enumerate(bottom_lines):
+        if len(set(c_line) & set(rows[l])):
+            for np, p in enumerate(rows[l]):
+                if p in c_line: rlevel.append(x_line.index(np)); break
+        # else:
+        #     raise Exception('bottom line level')
 
     # item bool
-    zitem = list()
-    for m in pixmenu:
-        zitem.append(bool(not len(set(c_arrow) & set(m))))
+    ritem = list()
+    for m in pix_menu:
+        ritem.append(bool(not len(set(c_arrow) & set(m))))
+
 
     # open menu bool
-    zopen = list()
-    for m in pixmenu:
+    ropen = list()
+    for m in pix_menu:
         if len(set(c_arrow) & set(m)):
-            i = [p for p, k in enumerate(m) if k in c_arrow][0]
-            vfr = len([p for p in m[i - 4:i + 5] if p in c_bg])
-            if vfr == 7: zopen.append(False)
-            if vfr == 5: zopen.append(True)
-            if vfr != 7 and vfr != 5: raise Exception('Detect open')
+            for c in c_arrow:
+                if c in m:
+                    i = m.index(c)
+                    p = len([p for p in m[i - 4:i + 5] if p in c_bg])
+                    if p == 7: ropen.append(False); break
+                    if p == 5: ropen.append(True); break
+                    if p != 7 and p != 5: raise Exception('open menu')
+                    break
         else:
-            zopen.append(False)
+            ropen.append(False)
+
+    if len({len(bottom_lines), len(rlevel), len(ritem), len(ropen)}) > 1: raise Exception('Length of the array')
+
+    rx = list(map(lambda l, i: x_item if i else field[0] + x_line[l] + 7, rlevel, ritem))
+    ry = list(map(lambda b: field[2] + (b - 12), bottom_lines))
+
+    rcoord = tuple(zip(rx, ry))
+    rgrid = tuple(zip(rlevel, ropen, ritem, rcoord))
 
     # scroll
-    up = False; down = False; spos = False
-    scl = [row[-10:-11:-1][0] for row in rows]
+    scl = [row[-10:-11:-1][0] for row in rows] # prepare scroll pix
+    scl = scl[4:-7] # up, down
 
-    if c_sp[0] in scl or c_sp[1] in scl:
+    for n, c in enumerate(scl):
+        if c in c_scl:
+            rpos = (408, field[2] + n + 4)
+            break
+        else:
+            rpos = (408, field[2])
 
-        for i in range(0, len(scl)):
-            if scl[i] in c_sud: up = i; break
-            if scl[i] in c_sp: up = i; break
+    rhview = len(scl) + 4
+    rhscl = len([p for p in scl if p in c_scl])
+    rupline = bottom_lines[0]
+    rdownline = bottom_lines[len(bottom_lines) - 1]
 
-        for i in range(len(scl) - 1, 0, -1):
-            if scl[i] in c_sud: down = i; break
-            if scl[i] in c_sp: down = i; break
+    rscroll = (rhview, rhscl, rupline, rdownline, rpos)
 
-        if not up or not down: raise Exception('Scroll range')
+    return rgrid, rscroll
 
-        for i in range(0, len(scl)):
-            if scl[i] in c_sp and scl[i + 1] in c_sp and scl[i + 2] in c_sp: spos = i; break
 
-        zscroll = {
-            'scl': len([p for p in scl if p in c_sp]),
-            'vpos': (field[2] + up, field[2] + down),
-            'spos': (x_scroll, field[2] + spos),
-            'topl': linepos[0],
-            'bottoml': linepos[len(linepos) - 1]
-        }
+def mouse(hwnd, coord, key, ud='', step=0):
+    x, y = coord
 
+    if  key == 'l':
+        down = win32con.WM_LBUTTONDOWN
+        up = win32con.WM_LBUTTONUP
+        wparam = win32con.MK_LBUTTON
+    elif key == 'r':
+        down = win32con.WM_RBUTTONDOWN
+        up = win32con.WM_RBUTTONUP
+        wparam = win32con.MK_RBUTTON
+    elif key == 'm':
+        down = win32con.WM_MBUTTONDOWN
+        up = win32con.WM_MBUTTONUP
+        wparam = win32con.MK_MBUTTON
+    elif key == 'w':
+        if  ud == 'u':
+            wparam = 0xFF880000
+        elif ud == 'd':
+            wparam = 0x780000
+        else:
+            wparam = 0x0
     else:
+        return
 
-        zscroll = {
-            'scl': 0,
-            'vpos': (field[2], field[3]),
-            'spos': (x_scroll, field[2]),
-            'topl': linepos[0],
-            'bottoml': linepos[len(linepos) - 1]
-        }
+    lparam = win32api.MAKELONG(x, y)
 
-    if len({len(linepos), len(zlevel), len(zopen), len(zitem)}) > 1: raise Exception('Length of the array')
-
-    global statedb
-    global scrolldb
-
-    for k in statedb.keys():
-        statedb[k].clear()
-
-    for i, y in enumerate(linepos):
-        x = x_item if zitem[i] else field[0] + x_line[zlevel[i]] + 7
-        y = field[2] + (y - 12)
-        statedb[zlevel[i]].append((zopen[i], zitem[i], (x, y)))
-
-    scrolldb.clear()
-    scrolldb = zscroll
+    win32gui.SendMessage(hwnd, win32con.WM_MOUSEMOVE, 0x0, lparam)
 
 
-def get_img(hwnd, coord):
+    if key != 'w':
+        win32gui.SendMessage(hwnd, down, wparam, lparam)
+        time.sleep(0.1)
+        win32gui.SendMessage(hwnd, up, 0x0, lparam)
+
+    if key == 'w':
+        for s in range(0, step):
+            time.sleep(0.1)
+            win32gui.SendMessage(hwnd, win32con.WM_MOUSEWHEEL, wparam, lparam)
+
+
+def screen(hwnd, coord):
 
     sx, ex, sy, ey = coord
     retlist = list()
